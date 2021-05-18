17c771fe071df901ffd80b6d0d5fc87dcf106841
XiangRn <cuadao1710@gmail.com>
2021-05-18 23:56:02 +0700

    My Leetcode exercises

---
+++ LeetCodeMaxConsecutiveOnes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMaxConsecutiveOnes
+{
+    class Program
+    {
+      static int FindMaxConsecutiveOnes(int[] nums)
+        {           
+            int count= 0; int m = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                    if (i == nums.Length - 1)
+                    {
+                        m = Math.Max(count, m);
+                    }
+                }
+                else
+                {
+                    if (count != 0)
+                    {
+                        m = Math.Max(count, m);
+                    }
+                    count = 0;
+                }
+            }           
+            return m;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+           int[] a = new int[] { 1, 1, 0, 1, 1, 1 };
+            
+            Console.WriteLine(FindMaxConsecutiveOnes(a));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetcodeFindNumberswithevennumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeFindNumberswithevennumbers
+{
+    class Program
+    {
+       static int FindNumbers(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {               
+                if (nums[i].ToString().Length % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; int n = Convert.ToInt32(Console.ReadLine());int[] a = new int[n];
+         
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            foreach (var item in a)
+            {
+                Console.Write(item +" ");
+            }
+            Console.Write("\n"); Console.WriteLine(FindNumbers(a));
+         
+            
+            Console.ReadLine();
+        }
+    }
+}
